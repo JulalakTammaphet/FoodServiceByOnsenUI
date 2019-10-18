31916f937bd7f9ccead7d0b1f5821f96705f7457
JulalakTammaphet <s6030213020@phuket.psu.ac.th>
2019-10-18 11:44:45 +0700

    edit and addcartpage

--- js/app.js
+++ js/app.js
@@ -35,6 +35,31 @@ document.addEventListener('init', function(event) {
         }
     }
 
+    if (page.id === 'loginPage') {
+        console.log("loginPage");
+
+        $("#gbtn").click(function() {
+            var provider = new firebase.auth.GoogleAuthProvider();
+            firebase.auth().signInWithPopup(provider).then(function(result) {
+                // This gives you a Google Access Token. You can use it to access the Google API.
+                var token = result.credential.accessToken;
+                // The signed-in user info.
+                var user = result.user;
+                content.load('FoodCategory.html');
+            }).catch(function(error) {
+                // Handle Errors here.
+                var errorCode = error.code;
+                var errorMessage = error.message;
+                // The email of the user's account used.
+                var email = error.email;
+                // The firebase.auth.AuthCredential type that was used.
+                var credential = error.credential;
+                // ...
+            });
+        });
+
+    }
+
 
 
 
@@ -81,3 +106,214 @@ document.addEventListener('init', function(event) {
         });
     }
 });
+
+
+// ************************************************
+// Food Cart API
+// ************************************************
+// if (page.id === 'ResturantMenupage') {
+var foodCart = (function() {
+    // =============================
+    // Private methods and propeties
+    // =============================
+    cart = [];
+
+    // Constructor
+    function Item(name, price, count) {
+        this.name = name;
+        this.price = price;
+        this.count = count;
+    }
+
+    // Save cart
+    function saveCart() {
+        sessionStorage.setItem('foodCart', JSON.stringify(cart));
+    }
+
+    // Load cart
+    function loadCart() {
+        cart = JSON.parse(sessionStorage.getItem('foodCart'));
+    }
+    if (sessionStorage.getItem("foodCart") != null) {
+        loadCart();
+    }
+
+
+    // =============================
+    // Public methods and propeties
+    // =============================
+    var obj = {};
+
+    // Add to cart
+    obj.addItemToCart = function(name, price, count) {
+            for (var item in cart) {
+                if (cart[item].name === name) {
+                    cart[item].count++;
+                    saveCart();
+                    return;
+                }
+            }
+            var item = new Item(name, price, count);
+            cart.push(item);
+            saveCart();
+        }
+        // Set count from item
+    obj.setCountForItem = function(name, count) {
+        for (var i in cart) {
+            if (cart[i].name === name) {
+                cart[i].count = count;
+                break;
+            }
+        }
+    };
+    // Remove item from cart
+    obj.removeItemFromCart = function(name) {
+        for (var item in cart) {
+            if (cart[item].name === name) {
+                cart[item].count--;
+                if (cart[item].count === 0) {
+                    cart.splice(item, 1);
+                }
+                break;
+            }
+        }
+        saveCart();
+    }
+
+    // Remove all items from cart
+    obj.removeItemFromCartAll = function(name) {
+        for (var item in cart) {
+            if (cart[item].name === name) {
+                cart.splice(item, 1);
+                break;
+            }
+        }
+        saveCart();
+    }
+
+    // Clear cart
+    obj.clearCart = function() {
+        cart = [];
+        saveCart();
+    }
+
+    // Count cart 
+    obj.totalCount = function() {
+        var totalCount = 0;
+        for (var item in cart) {
+            totalCount += cart[item].count;
+        }
+        return totalCount;
+    }
+
+    // Total cart
+    obj.totalCart = function() {
+        var totalCart = 0;
+        for (var item in cart) {
+            totalCart += cart[item].price * cart[item].count;
+        }
+        return Number(totalCart.toFixed(2));
+    }
+
+    // List cart
+    obj.listCart = function() {
+        var cartCopy = [];
+        for (i in cart) {
+            item = cart[i];
+            itemCopy = {};
+            for (p in item) {
+                itemCopy[p] = item[p];
+
+            }
+            itemCopy.total = Number(item.price * item.count).toFixed(2);
+            cartCopy.push(itemCopy)
+        }
+        return cartCopy;
+    }
+
+    // cart : Array
+    // Item : Object/Class
+    // addItemToCart : Function
+    // removeItemFromCart : Function
+    // removeItemFromCartAll : Function
+    // clearCart : Function
+    // countCart : Function
+    // totalCart : Function
+    // listCart : Function
+    // saveCart : Function
+    // loadCart : Function
+    return obj;
+})();
+
+
+// *****************************************
+// Triggers / Events
+// ***************************************** 
+// Add item
+$('.add-to-cart').click(function(event) {
+    event.preventDefault();
+    var name = $(this).data('name');
+    var price = Number($(this).data('price'));
+    foodCart.addItemToCart(name, price, 1);
+    displayCart();
+});
+
+// Clear items
+$('.clear-cart').click(function() {
+    foodCart.clearCart();
+    displayCart();
+});
+
+
+function displayCart() {
+    var cartArray = foodCart.listCart();
+    var output = "";
+    for (var i in cartArray) {
+        output += "<tr>" +
+            "<td>" + cartArray[i].name + "</td>" +
+            "<td>(" + cartArray[i].price + ")</td>" +
+            "<td><div class='input-group'><button class='minus-item input-group-addon btn btn-primary' data-name=" + cartArray[i].name + ">-</button>" +
+            "<input type='number' class='item-count form-control' data-name='" + cartArray[i].name + "' value='" + cartArray[i].count + "'>" +
+            "<button class='plus-item btn btn-primary input-group-addon' data-name=" + cartArray[i].name + ">+</button></div></td>" +
+            "<td><button class='delete-item btn btn-danger' data-name=" + cartArray[i].name + ">X</button></td>" +
+            " = " +
+            "<td>" + cartArray[i].total + "</td>" +
+            "</tr>";
+    }
+    $('.show-cart').html(output);
+    $('.total-cart').html(foodCart.totalCart());
+    $('.total-count').html(foodCart.totalCount());
+}
+
+// Delete item button
+
+$('.show-cart').on("click", ".delete-item", function(event) {
+    var name = $(this).data('name')
+    foodCart.removeItemFromCartAll(name);
+    displayCart();
+})
+
+
+// -1
+$('.show-cart').on("click", ".minus-item", function(event) {
+        var name = $(this).data('name')
+        foodCart.removeItemFromCart(name);
+        displayCart();
+    })
+    // +1
+$('.show-cart').on("click", ".plus-item", function(event) {
+    var name = $(this).data('name')
+    foodCart.addItemToCart(name);
+    displayCart();
+})
+
+// Item count input
+$('.show-cart').on("change", ".item-count", function(event) {
+    var name = $(this).data('name');
+    var count = Number($(this).val());
+    foodCart.setCountForItem(name, count);
+    displayCart();
+});
+
+displayCart();
+// }
